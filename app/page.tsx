853369c0cf5575330f7fa8ff2190b5ab02c44392
GandonioBandera <maximkanetu@yandex.ru>
2024-10-20 18:49:20 +0300

    фронтенд обновил

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
       <header>
         <li><a href="anime">Аниме</a></li>
         <li><a href="manga">Манга</a></li>
-        <li><a href="serials">Сериалы</a></li>
+        <li><a href="series">Сериалы</a></li>
         <li><a href="about">О нас</a></li>
       </header>);
 }
---
+++ app/series/page.tsx
@@ -0,0 +1,9 @@
+export default function series() {
+    return (
+        <header>
+            <li><a href="anime">Аниме</a></li>
+            <li><a href="manga">Манга</a></li>
+            <li><a href="series">Сериалы</a></li>
+            <li><a href="about">О нас</a></li>
+        </header>);
+}
